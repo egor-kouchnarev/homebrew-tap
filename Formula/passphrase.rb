250e7082e22bf16468d0a11f8c8640b92d050cf9
Egor Kouchnarev <69385806+egor-kouchnarev@users.noreply.github.com>
2024-06-02 21:36:08 +1000

    v0.3.1-alpha

--- Formula/passphrase.rb
+++ Formula/passphrase.rb
@@ -1,8 +1,8 @@
 class Passphrase < Formula
-  desc "A command-line random passphrase generator"
+  desc "An on-device, command-line random passphrase generator"
   homepage "https://github.com/egor-kouchnarev/homebrew-passphrase"
-  url "https://github.com/egor-kouchnarev/homebrew-passphrase/archive/refs/tags/v0.3.0-alpha.tar.gz"
-  sha256 "50ee58eb5c53570a1e24cd600643abd748b9d7517084dc370a24b7bf42932df0"
+  url "https://github.com/egor-kouchnarev/homebrew-passphrase/archive/refs/tags/v0.3.1-alpha.tar.gz"
+  sha256 "d7ccd94020b7ff911e067d8ae55f82662ef3c261ed6e5cca23dacb4e676d9eff"
   license "MIT"
 
   depends_on "make" => :build
--- Formula/password.rb
+++ Formula/password.rb
@@ -1,8 +1,8 @@
 class Password < Formula
-  desc "A command-line random password generator"
+  desc "An on-device, command-line random password generator"
   homepage "https://github.com/egor-kouchnarev/homebrew-password"
-  url "https://github.com/egor-kouchnarev/homebrew-password/archive/refs/tags/v0.3.0-alpha.tar.gz"
-  sha256 "5523a27cf8610cd9270b95ec20c054cfdf2b9eab40edef9963e450a4a97c084e"
+  url "https://github.com/egor-kouchnarev/homebrew-password/archive/refs/tags/v0.3.1-alpha.tar.gz"
+  sha256 "f80d9b4a5b7704f93c5067fbded394ed91026c4cfa610a50a820637db9a14af3"
   license "MIT"
 
   depends_on "make" => :build
