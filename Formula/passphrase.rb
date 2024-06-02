151166e597639f0d2e49d203d47555818e41ef02
Egor Kouchnarev <69385806+egor-kouchnarev@users.noreply.github.com>
2024-06-02 17:47:35 +1000

    Update password and passphrase to v1.0.0

--- Formula/passphrase.rb
+++ Formula/passphrase.rb
@@ -1,8 +1,8 @@
 class Passphrase < Formula
   desc "A passphrase generator written in C"
   homepage "https://github.com/egor-kouchnarev/homebrew-passphrase"
-  url ""
-  sha256 ""
+  url "https://github.com/egor-kouchnarev/homebrew-passphrase/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "74070eb4a04eb325234d0c5bdc5efe7b3de2fa094472909de14c0f9896c5190d"
   license "MIT"
 
   depends_on "make" => :build
--- Formula/password.rb
+++ Formula/password.rb
@@ -2,7 +2,7 @@ class Password < Formula
   desc "A password generator written in C"
   homepage "https://github.com/egor-kouchnarev/homebrew-password"
   url "https://github.com/egor-kouchnarev/homebrew-password/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3748df3e4e3848df29291f5afbaa70778256fa6eecad91994dcfb3b087305437"
+  sha256 "87b0b85e9beaf582b725d0571f7b04e97b885beb40ad2ba13dcb2a612b9b0fe9"
   license "MIT"
 
   depends_on "make" => :build
