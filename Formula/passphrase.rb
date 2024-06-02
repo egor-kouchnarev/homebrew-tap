dddd49893493a97eddce7e4a5da6fde8103eceb8
Egor Kouchnarev <69385806+egor-kouchnarev@users.noreply.github.com>
2024-06-02 21:19:18 +1000

    v0.3.0-alpha

--- Formula/passphrase.rb
+++ Formula/passphrase.rb
@@ -1,5 +1,5 @@
 class Passphrase < Formula
-  desc "A passphrase generator written in C"
+  desc "A command-line random passphrase generator"
   homepage "https://github.com/egor-kouchnarev/homebrew-passphrase"
   url "https://github.com/egor-kouchnarev/homebrew-passphrase/archive/refs/tags/v0.3.0-alpha.tar.gz"
   sha256 "50ee58eb5c53570a1e24cd600643abd748b9d7517084dc370a24b7bf42932df0"
--- Formula/password.rb
+++ Formula/password.rb
@@ -1,5 +1,5 @@
 class Password < Formula
-  desc "A password generator written in C"
+  desc "A command-line random password generator"
   homepage "https://github.com/egor-kouchnarev/homebrew-password"
   url "https://github.com/egor-kouchnarev/homebrew-password/archive/refs/tags/v0.3.0-alpha.tar.gz"
   sha256 "5523a27cf8610cd9270b95ec20c054cfdf2b9eab40edef9963e450a4a97c084e"
