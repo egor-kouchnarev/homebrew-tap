48fe3348a37d6be61ec0bda49a4d5af094d52038
Egor Kouchnarev <69385806+egor-kouchnarev@users.noreply.github.com>
2024-06-02 16:57:54 +1000

    Create password.rb

---
+++ password.rb
@@ -0,0 +1,18 @@
+class Password < Formula
+  desc "A simple password generator written in C"
+  homepage "https://github.com/egor-kouchnarev/homebrew-password"
+  url "https://github.com/egor-kouchnarev/homebrew-password/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "3748df3e4e3848df29291f5afbaa70778256fa6eecad91994dcfb3b087305437"
+  license "MIT"
+
+  depends_on "make" => :build
+
+  def install
+    system "make"
+    system "make", "install", "DESTDIR=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/password", "--version"
+  end
+end
